01ed33d953b00599fa22ea49e904e5a29cbaa092
jiminyun <jiminyun7@gmail.com>
2019-04-05 20:48:46 -0400

    Updates

---
+++ precache-manifest.7df0209d1ddde4450b91ba7c8a61c907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0325ebbca41c285b593c",
+    "url": "/jimflix/static/js/main.0325ebbc.chunk.js"
+  },
+  {
+    "revision": "ae1f3d87805a4e947f5c",
+    "url": "/jimflix/static/js/1.ae1f3d87.chunk.js"
+  },
+  {
+    "revision": "e3db734435a0ef631236",
+    "url": "/jimflix/static/js/runtime~main.e3db7344.js"
+  },
+  {
+    "revision": "b3bd7196687a10b469e56ba971d06575",
+    "url": "/jimflix/static/media/logo-valentines.b3bd7196.png"
+  },
+  {
+    "revision": "2a1eb55edb9d2cca33fd65f80bfce4b4",
+    "url": "/jimflix/index.html"
+  }
+];
